fdfc45d2929c914860d782225ac3ce6ad07515be
Noriah Williams <willnoriah23@gmail.com>
2017-09-15 20:41:12 -0400

    Homework, almost there..

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,120 +1,121 @@
-// Timer
-var number = 100;
+$('#start').on('click', function(){
+
+    startTimer();
+    createQuestionElement();
+   
+
+});
+
+
+function createQuestionElement() {
+    //console.log(questions);
+    var questionsElement = $("<div><h1>test</h1></div>");
+    // $("#questionsContainer").append(questionsElement);
+    $.each(questions, function(index,val){
+      console.log(index, val);
+      //console.log(val.question);
+      var radioStuff = $('<div><h2>'+val.question+'</h2></div>');
+      $('#questionsContainer').append(radioStuff).append(createRadios(index));
+    });
+}
+
+function createRadios(index) {
+    // var radioList = $('<ul>');
+    //var item;
+    // console.log(index)
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+       
+      // item = $('<li>');
+      input += '<input type="radio" name="choices" value=' + questions[index].choices[i] + ' />';
+
+      input += questions[index].choices[i] + "<br>";
+      //item.append(input);
+      //radioList.append(item);
+    }
+    return input;
+  }
+
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+
+var number = 60;
  
 var timerId;
 
-function run() {
-    timerId = setInterval(decrement, 1000);
-}
-// Makes timer count down
-function decrement() {
-    number--;
-
-    $("#show-timer").html("<h2>" + number + "</h2>");
-
-    if (number == 0) {
-        stop();
-
-        alert("Time Up!");
+function startTimer() {
+    var decrement = function() {
+        //console.log("Line 40: ", number );
+        if (number == 0) {
+            stop();
+            alert("Time Up!");
+        }
+        number--;
+        $("#timer").html("<h2>" + number + "</h2>");
     }
+    timerId = setInterval(decrement, 1000);
 }
 
 function stop() {
     clearInterval(timerId);
 }
 
-run();
 
-// My attempt at posting my questions to the HTML
-function createQuestionElement(index) {
+    var questions = [
+    {
 
-    var qElement = $('<div>', {
-        id: 'question'
-    });
-
-    var question = $('<p>').append(questions[index].question);
-    qElement.append(question);
-
-    var radioButtons = createRadios(index);
-    qElement.append(radioButtons);
-
-    return qElement;
-}
-
-//Questions
-var questions = [{
-
-    question: "Beyoncé was named after what family member?",
-    choices: ["Paternal grandmother", "Her mother’s maiden name", "A Creole seasoning", "A Texas city"]
+    question: "What was Beyoncé named after?",
+    choices: ["Paternal grandmother", "Her mother’s maiden name", "A Creole seasoning", "A Texas city"],
     correctAnswer: "Her mother’s maiden name"
      },
      {
     question: "How many solo albums does Beyonce have?",
-    choices: ["8", "2", "6", "7"]
+    choices: ["8", "2", "6", "7"],
     correctAnswer: "6"
     },
     {
     question: "What female music group was Beyoncé a member of?",
-    choices: ["SWV", "Destiny's Child", "TLC", "The Supremes"]; 
+    choices: ["SWV", "Destiny's Child", "TLC", "The Supremes"],
     correctAnswer: "Destiny's Child"
     },
     {
     question: "What is the name of Beyoncé’s only son?",
-    choices: ["Mister", "Jay", "Knowles", "Sir"];
+    choices: ["Mister", "Jay", "Knowles", "Sir"],
     correctAnswer: "Sir"
     },
     {
     question: "What is the name of Beyoncé charity foundation?",
-    choices: ["BEYGOOD", "Bey Gives Back", "‘Yonce Fund", "B & J Money"];
+    choices: ["BEYGOOD", "Bey Gives Back", "‘Yonce Fund", "B & J Money"],
     correctAnswer: "BEYGOOD"
     },
     {
     question: "How many times has Beyoncé been nominated for a Grammy?",
-    choices: ["101", "62", "25", "12"];
+    choices: ["101", "62", "25", "12"],
     correctAnswer: "62"
     },
     {
     question: "Destiny's Child recorded &quot;Independent Women Part I&quot;, which appeared on the soundtrack to what 2000 film?",
-    choices: ["Charlie's Angels", "Coyote Ugly", "Remember The Titans", "Bring It On"];
+    choices: ["Charlie's Angels", "Coyote Ugly", "Remember The Titans", "Bring It On"],
     correctAnswer: "Charlie's Angels"
     },
     {
-    question: "What world famous supermodel has the same name as Beyoncé's middle name?"
-    choices: ["Naomi Campbell", "Kate Moss", "Gisele", "Adriana Lima"];
+    question: "What world famous supermodel has the same first name as Beyoncé's middle name?",
+    choices: ["Naomi Campbell", "Kate Moss", "Gisele", "Adriana Lima"],
     correctAnswer: "Gisele"
     },
     {
-    question: "What word, popularised by Beyoncé, was added to the Oxford English Dictionary?",
-    choices: ["Bootylicious", "Fleek", "Clickbait", "Swag"];
+    question: "What word, popularised by Destiny's Child, was added to the Oxford English Dictionary in 2004?",
+    choices: ["Bootylicious", "Fleek", "Clickbait", "Swag"],
     correctAnswer: "Bootylicious"
     },
     {
-    question: "Which artist was not featured on Beyoncé's self-titled album?"
-    choices: ["Drake", "Frank Ocean", "Jay-Z", "Rihanna"]
+    question: "Which artist was not featured on Beyoncé's self-titled album?",
+    choices: ["Drake", "Frank Ocean", "Jay-Z", "Rihanna"],
     correctAnswer: "Rihanna"    
-    };
+    }
 ];
 
-// My attempt at posting my questions to the HTML with radio buttons
-function createRadios(index) {
-    var radioList = $('<ul>');
-    var item;
-    var input = '';
-    for (var i = 0; i < questions[index].choices.length; i++) {
-      item = $('<li>');
-      input = '<input type="radio" name="answer" value=' + i + ' />';
-      input += questions[index].choices[i];
-      item.append(input);
-      radioList.append(item);
-    }
-    return radioList;
-  }
- 
-
-function choose() {
-    selections[questionCounter] = +$('input[name="answer"]:checked').val();
-  }
-  
 function displayScore() {
     var score = $('<p>',{id: 'question'});
     
@@ -129,6 +130,3 @@ function displayScore() {
                  questions.length + ' right!');
     return score;
   }
-
-
-        
