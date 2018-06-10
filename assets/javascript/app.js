bb3682d547fc039aad70197fad668c2538032ce4
Noriah Williams <willnoriah23@gmail.com>
2018-06-10 19:42:56 -0400

    Rewrote functions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,76 @@
+var questions = [
+{
+
+question: "What was Beyoncé named after?",
+choices: ["Paternal grandmother", "Her mother’s maiden name", "A Creole seasoning", "A Texas city"],
+correctAnswer: "Her mother’s maiden name"
+ },
+ {
+question: "How many solo albums does Beyonce have?",
+choices: ["8", "2", "6", "7"],
+correctAnswer: "6"
+},
+{
+question: "What female music group was Beyoncé a member of?",
+choices: ["SWV", "Destiny's Child", "TLC", "The Supremes"],
+correctAnswer: "Destiny's Child"
+},
+{
+question: "What is the name of Beyoncé’s only son?",
+choices: ["Mister", "Jay", "Knowles", "Sir"],
+correctAnswer: "Sir"
+},
+{
+question: "What is the name of Beyoncé charity foundation?",
+choices: ["BEYGOOD", "Bey Gives Back", "‘Yonce Fund", "B & J Money"],
+correctAnswer: "BEYGOOD"
+},
+{
+question: "How many times has Beyoncé been nominated for a Grammy?",
+choices: ["101", "62", "25", "12"],
+correctAnswer: "62"
+},
+{
+question: "Destiny's Child recorded &quot;Independent Women Part I&quot;, which appeared on the soundtrack to what 2000 film?",
+choices: ["Charlie's Angels", "Coyote Ugly", "Remember The Titans", "Bring It On"],
+correctAnswer: "Charlie's Angels"
+},
+{
+question: "What world famous supermodel has the same first name as Beyoncé's middle name?",
+choices: ["Naomi Campbell", "Kate Moss", "Gisele", "Adriana Lima"],
+correctAnswer: "Gisele"
+},
+{
+question: "What word, popularised by Destiny's Child, was added to the Oxford English Dictionary in 2004?",
+choices: ["Bootylicious", "Fleek", "Clickbait", "Swag"],
+correctAnswer: "Bootylicious"
+},
+{
+question: "Which artist was not featured on Beyoncé's self-titled album?",
+choices: ["Drake", "Frank Ocean", "Jay-Z", "Rihanna"],
+correctAnswer: "Rihanna"    
+}
+];
+
+
 $('#start').on('click', function(){
 
     startTimer();
     createQuestionElement();
+    done();
 
 });
 
+$(document).on("click", "#done", function() {
+  game.done();
+});
+
+
+var game = {
+
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
 
 function createQuestionElement() {
     //console.log(questions);
@@ -16,6 +82,8 @@ function createQuestionElement() {
       var radioStuff = $('<div><h2>'+val.question+'</h2></div>');
       $('#questionsContainer').append(radioStuff).append(createRadios(index));
     });
+
+    panel.append("<button id='done'>Done</button>");
 }
 
 function createRadios(index) {
@@ -30,9 +98,9 @@ function createRadios(index) {
       //radioList.append(item);
     }
     return input;
-  }
+}
 
-var number = 60;
+var number = 90;
  
 var timerId;
 
@@ -40,17 +108,13 @@ function startTimer() {
     var decrement = function() {
         //console.log("Line 40: ", number );
         if (number == 0) {
-            stop();
-            // alert("Time Up!");
+          console.log("Time Up!");
+          displayScore();
         }
         number--;
         $("#timer").html("<h2>" + number + "</h2>");
     }
     timerId = setInterval(decrement, 1000);
-}
-
-function stop() {
-    clearInterval(timerId);
 }
 
 $("#submit").on('click', function() {
@@ -67,67 +131,105 @@ function getCheckedValue(choices) {
 function getScore(){
   var score = 0;
   for (var i=0; i<tot; i++)
-    if(getCheckedValue("question"+i)=== correctAnswer[i]) score += 1; 
+    if (getCheckedValue("question"+i) === correctAnswer[i]) {
+        score += 1; 
+    } else {
+
+    }    
   return score;
 }
 
-// I want to use <a href> and target a new page, to display the correct and incorrect answers
+function done() {
 
-    var questions = [
-    {
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
 
-    question: "What was Beyoncé named after?",
-    choices: ["Paternal grandmother", "Her mother’s maiden name", "A Creole seasoning", "A Texas city"],
-    correctAnswer: "Her mother’s maiden name"
-     },
-     {
-    question: "How many solo albums does Beyonce have?",
-    choices: ["8", "2", "6", "7"],
-    correctAnswer: "6"
-    },
-    {
-    question: "What female music group was Beyoncé a member of?",
-    choices: ["SWV", "Destiny's Child", "TLC", "The Supremes"],
-    correctAnswer: "Destiny's Child"
-    },
-    {
-    question: "What is the name of Beyoncé’s only son?",
-    choices: ["Mister", "Jay", "Knowles", "Sir"],
-    correctAnswer: "Sir"
-    },
-    {
-    question: "What is the name of Beyoncé charity foundation?",
-    choices: ["BEYGOOD", "Bey Gives Back", "‘Yonce Fund", "B & J Money"],
-    correctAnswer: "BEYGOOD"
-    },
-    {
-    question: "How many times has Beyoncé been nominated for a Grammy?",
-    choices: ["101", "62", "25", "12"],
-    correctAnswer: "62"
-    },
-    {
-    question: "Destiny's Child recorded &quot;Independent Women Part I&quot;, which appeared on the soundtrack to what 2000 film?",
-    choices: ["Charlie's Angels", "Coyote Ugly", "Remember The Titans", "Bring It On"],
-    correctAnswer: "Charlie's Angels"
-    },
-    {
-    question: "What world famous supermodel has the same first name as Beyoncé's middle name?",
-    choices: ["Naomi Campbell", "Kate Moss", "Gisele", "Adriana Lima"],
-    correctAnswer: "Gisele"
-    },
-    {
-    question: "What word, popularised by Destiny's Child, was added to the Oxford English Dictionary in 2004?",
-    choices: ["Bootylicious", "Fleek", "Clickbait", "Swag"],
-    correctAnswer: "Bootylicious"
-    },
-    {
-    question: "Which artist was not featured on Beyoncé's self-titled album?",
-    choices: ["Drake", "Frank Ocean", "Jay-Z", "Rihanna"],
-    correctAnswer: "Rihanna"    
-    }
-];
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
 
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
 
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+function result() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+};
 
 // function returnScore(){
 //   alert("Your score is "+ getScore() +"/"+ tot);
